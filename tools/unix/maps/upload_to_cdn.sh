fd6cd9931cf951716d4524316a137ced48b7c22c
Konstantin Pastbin <konstantin.pastbin@gmail.com>
2026-04-04 13:36:52 +0700

    [mapgen] Add it1 to CDN upload
    Signed-off-by: Konstantin Pastbin <konstantin.pastbin@gmail.com>

--- tools/unix/maps/upload_to_cdn.sh
+++ tools/unix/maps/upload_to_cdn.sh
@@ -7,7 +7,7 @@ set -e -u
 if [ $# -eq 0 ]; then
   echo "Usage: upload_to_cdn.sh MAPS_PATH"
   echo "e.g. sudo upload_to_cdn.sh osm-maps/2025_09_06__09_48_08/250906"
-  echo "uploads are run in parallel to us2,ru1,fi1,de1 servers,"
+  echo "uploads are run in parallel to all CDN nodes/servers,"
   echo "subsequent runs will update only missing/differing files,"
   echo "so its fine to run second time to ensure there were no incomplete transfers"
   echo "or to run on an unfinished generation first and then again after its fully finished."
@@ -59,6 +59,9 @@ remove_old $DE1 6
 FR1="fr1:/data/maps"
 remove_old $FR1 6
 
+IT1="it1:maps"
+remove_old $IT1 6
+
 US2="us2:comaps-map-files/maps"
 echo "Skipping $US2 cleanup (keep all versions)"
 echo "  Versions present: $(rclone lsf --dirs-only --dir-slash=false --max-depth=1 $US2 | sort -r | tr '\n' ' ')"
@@ -80,6 +83,9 @@ rclone copy --include "*.{mwm,txt,sig}" $DIR $DE1/$MAPS &
 
 echo "Uploading to fr1"
 rclone copy --include "*.{mwm,txt,sig}" $DIR $FR1/$MAPS &
+
+echo "Uploading to it1"
+rclone copy --include "*.{mwm,txt,sig}" $DIR $IT1/$MAPS &
 
 # us1 is not used for maps atm
 # rclone lsd us1:/home/dh_zzxxrk/cdn-us-1.comaps.app/maps
@@ -103,6 +109,9 @@ rclone copy -v --include "*.{mwm,txt,sig}" $DIR $DE1/$MAPS
 echo "$FR1 status:"
 rclone copy -v --include "*.{mwm,txt,sig}" $DIR $FR1/$MAPS
 
+echo "$IT1 status:"
+rclone copy -v --include "*.{mwm,txt,sig}" $DIR $IT1/$MAPS
+
 echo "Upload complete"
 
 echo "us2 versions present: $(rclone lsf --dirs-only --dir-slash=false --max-depth=1 $US2 | sort -r | tr '\n' ' ')"
@@ -110,5 +119,6 @@ echo "ru1 versions present: $(rclone lsf --dirs-only --dir-slash=false --max-dep
 echo "fi1 versions present: $(rclone lsf --dirs-only --dir-slash=false --max-depth=1 $FI1 | sort -r | tr '\n' ' ')"
 echo "de1 versions present: $(rclone lsf --dirs-only --dir-slash=false --max-depth=1 $DE1 | sort -r | tr '\n' ' ')"
 echo "fr1 versions present: $(rclone lsf --dirs-only --dir-slash=false --max-depth=1 $FR1 | sort -r | tr '\n' ' ')"
+echo "it1 versions present: $(rclone lsf --dirs-only --dir-slash=false --max-depth=1 $IT1 | sort -r | tr '\n' ' ')"
 
 echo "DONE"
