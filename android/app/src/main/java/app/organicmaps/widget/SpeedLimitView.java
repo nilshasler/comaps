organicmaps/organicmaps

[android-auto] Show speed limit
Signed-off-by: Andrei Shkrob <github@shkrob.dev>

android/app/src/main/java/app/organicmaps/widget/SpeedLimitView.java
@@ -66,11 +66,17 @@ public class SpeedLimitView extends View
   private String mSpeedLimitStr = "0";
   private boolean mAlert = false;
 
+  public SpeedLimitView(Context context)
+  {
+    this(context, null);
+  }
+
   public SpeedLimitView(Context context, @Nullable AttributeSet attrs)
   {
     super(context, attrs);
 
-    try (TypedArray data = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SpeedLimitView, 0, 0))
+    try (TypedArray data = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SpeedLimitView, 0,
+                                                                     R.style.MwmWidget_SpeedLimit))
     {
       mBackgroundColor =
           data.getColor(R.styleable.SpeedLimitView_speedLimitBackgroundColor, DefaultValues.BACKGROUND_COLOR);
@@ -115,6 +121,16 @@ public class SpeedLimitView extends View
     }
 
     invalidate();
+  }
+
+  public int getSpeedLimit()
+  {
+    return mSpeedLimit;
+  }
+
+  public boolean isAlert()
+  {
+    return mAlert;
   }
 
   @Override

nilshasler/comaps

android/app/src/main/java/app/organicmaps/widget/SpeedLimitView.java
@@ -16,6 +16,11 @@ public class SpeedLimitView extends BaseSignView
   private final int unlimitedBorderColor;
   private final int unlimitedStripeColor;
 
+  public SpeedLimitView(Context ctx)
+  {
+    this(ctx, null);
+  }
+
   public SpeedLimitView(Context ctx, @Nullable AttributeSet attrs)
   {
     super(ctx, attrs);
@@ -23,7 +28,8 @@ public class SpeedLimitView extends BaseSignView
     setBorderWidthRatio(0.2f);
     setBorderInsetRatio(0.05f);
 
-    try (TypedArray styleAttrs = ctx.getTheme().obtainStyledAttributes(attrs, R.styleable.SpeedLimitView, 0, 0))
+    try (TypedArray styleAttrs = ctx.getTheme().obtainStyledAttributes(attrs, R.styleable.SpeedLimitView, 0,
+                                                                       R.style.MwmWidget_SpeedLimit))
     {
       final int bgColor =
           styleAttrs.getColor(R.styleable.SpeedLimitView_speedLimitBackgroundColor, DefaultValues.BACKGROUND_COLOR);
@@ -64,17 +70,22 @@ public class SpeedLimitView extends BaseSignView
     invalidate();
   }
 
+  public int getSpeedLimit()
+  {
+    return mSpeedLimit;
+  }
+
+  @Override
+  public boolean isAlert()
+  {
+    return mAlert;
+  }
+
   @Nullable
   @Override
   protected String getValueString()
   {
     return (mSpeedLimit > 0 ? mSpeedStr : null);
-  }
-
-  @Override
-  protected boolean isAlert()
-  {
-    return mAlert;
   }
 
   @Override