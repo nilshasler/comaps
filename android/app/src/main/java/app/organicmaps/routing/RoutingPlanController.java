447e7a61d5b1ad010cef225d0d384c036691910d
Nils Hasler <hasler@thecaptury.com>
2026-07-14 10:27:59 +0200

    clickable banner background?

--- android/app/src/main/java/app/organicmaps/routing/RoutingPlanController.java
+++ android/app/src/main/java/app/organicmaps/routing/RoutingPlanController.java
@@ -106,7 +106,8 @@ public class RoutingPlanController extends ToolbarController
     mRoutingBottomMenuController = RoutingBottomMenuController.newInstance(requireActivity(), mFrame, listener);
 
     mRoutingOptionsBanner = mFrame.findViewById(R.id.routing_options_banner);
-    mRoutingOptionsBanner.setOnClickListener(v -> RoutingOptionsActivity.start(requireActivity(), startRoutingOptionsForResult));
+    LinearLayout container = mRoutingOptionsBanner.findViewById(R.id.routing_options_buttons_container);
+    container.setOnClickListener(v -> RoutingOptionsActivity.start(requireActivity(), startRoutingOptionsForResult));
     View btn = mFrame.findViewById(R.id.routing_options_btn);
     btn.setOnClickListener(v -> RoutingOptionsActivity.start(requireActivity(), startRoutingOptionsForResult));
 
