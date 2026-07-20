330f1273b5ac34fbbc52202d114804a9b0ab8913
Nils Hasler <hasler@thecaptury.com>
2026-07-20 09:55:24 +0200

    import

--- android/app/src/main/java/app/organicmaps/routing/RoutingPlanInplaceController.java
+++ android/app/src/main/java/app/organicmaps/routing/RoutingPlanInplaceController.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
