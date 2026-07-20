b3c431c2b2d601e77cadc9574c1673ae22e4d0b8
Nils Hasler <hasler@thecaptury.com>
2026-07-20 09:41:38 +0200

    more logging

--- android/app/src/main/java/app/organicmaps/routing/RoutingPlanInplaceController.java
+++ android/app/src/main/java/app/organicmaps/routing/RoutingPlanInplaceController.java
@@ -33,7 +33,7 @@ public class RoutingPlanInplaceController extends RoutingPlanController
   private static final String TAG = RoutingPlanInplaceController.class.getSimpleName();
   public void show(final boolean show)
   {
-    Logger.d(TAG, "show=" + show + " mAnimator=" + mAnimator, new Exception("Visibility Stacktrace"));
+    Logger.d(TAG, "show=" + show + " height=" + getFrame().getHeight() +" vis=" + getFrame().getVisibility() == View.VISIBLE ? "VISIBLE" : "GONE" + " Y=" + getFrame().getY(), new Exception("Visibility Stacktrace"));
     if (mAnimator != null)
     {
       mAnimator.cancel();
@@ -61,7 +61,7 @@ public class RoutingPlanInplaceController extends RoutingPlanController
   @Nullable
   private ValueAnimator animateFrame(final boolean show, final @Nullable Runnable completion)
   {
-    Logger.d(TAG, "animateFrame show=" + show + " height=" + checkFrameHeight() +" vis=" + UiUtils.isVisible(getFrame()));
+    Logger.d(TAG, "animateFrame show=" + show + " height=" + getFrame().getHeight() +" vis=" + getFrame().getVisibility() == View.VISIBLE ? "VISIBLE" : "GONE" + " Y=" + getFrame().getY());
     if (!checkFrameHeight())
     {
       getFrame().post(() -> animateFrame(show, completion));
