1e425c40b76105a9fecaf9c713abfd616637f435
Nils Hasler <hasler@thecaptury.com>
2026-07-26 11:53:56 +0200

    add logging

--- android/app/src/main/java/app/organicmaps/routing/SpyView.java
+++ android/app/src/main/java/app/organicmaps/routing/SpyView.java
@@ -42,4 +42,34 @@ public class SpyView extends LinearLayout {
         super.onDetachedFromWindow();
         Logger.d("SpyView", "Root view was DETACHED from window!", new Exception("Detachment Stacktrace"));
     }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+      super.onDraw(canvas);
+      
+      // Check if the view actually has dimensions
+      int width = getWidth();
+      int height = getHeight();
+      float alpha = getAlpha();
+  
+      Logger.d("SpyView", "onDraw called. Dimensions: " + width + "x" + height + " | Alpha: " + alpha);
+  
+      if (getParent() instanceof ViewGroup) {
+          ViewGroup parent = (ViewGroup) getParent();
+          int myIndex = parent.indexOfChild(this);
+          int totalChildren = parent.getChildCount();
+          
+          Logger.d("SpyView", "My position in parent: " + myIndex + " out of " + totalChildren);
+          
+          // If myIndex is less than (totalChildren - 1), siblings are drawn AFTER (on top of) this view
+          if (myIndex < totalChildren - 1) {
+              for (int i = myIndex + 1; i < totalChildren; i++) {
+                  View sibling = parent.getChildAt(i);
+                  Logger.d("SpyView", "Sibling above me: " + sibling.getClass().getSimpleName() 
+                          + " | ID: " + sibling.getId() 
+                          + " | Visibility: " + sibling.getVisibility());
+              }
+          }
+      }
+  }
 }
